97a9280e2aa8a2a35859488026d58633691d3c9f
Tokuune <t.tokuo11@gmail.com>
2025-10-25 21:04:16 -0700

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編 課題_011</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $item_data = [
+            '名前' => '玉ねぎ',
+            '値段' => 200,
+            '産地' => '北海道',
+        ];
+
+        foreach ($item_data as $key => $value) {
+            echo "{$key}：{$value}<br>";
+        }
+        ?>
+    </p>
+</body>
+
+</html>

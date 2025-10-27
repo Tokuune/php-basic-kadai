3dc49b567cef8756bf0e2c2d3df64bc96d77c4de
Tokuune <t.tokuo11@gmail.com>
2025-10-27 00:06:08 -0700

    Update sort.php

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -12,22 +12,24 @@
         $nums = [15, 4, 18, 23, 10];
         function sort_2way(array &$array, bool $order): void {
           if ($order === TRUE) {
-            echo '昇順にソートします。';
+            echo '昇順にソートします。<br>';
             sort($array);   // 昇順
             }
           else {
-            echo '降順でソートします。';
+            echo '降順でソートします。<br>';
             rsort($array);  // 降順
           }
         }
 
           sort_2way($nums, TRUE);  // 昇順
-          foreach($nums as $num);
+          foreach($nums as $num){
           echo $num . '<br>';
+          }
 
           sort_2way($nums, FALSE); // 降順
-          foreach($nums as $num);
+          foreach($nums as $num){
           echo $num . '<br>';
+          }
         ?>
     </p>
     
